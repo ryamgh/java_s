ac8ebb254b0a82f60e2e78516f0d540212a6a1c4
Ram Ghimire <ramghimire@192.168.0.108>
2025-11-26 22:04:19 +0545

    updated

---
+++ block_And_BlockScope.js
@@ -0,0 +1,17 @@
+
+
+let a= 24;
+
+
+function demonstrateBlockScope (){
+    var b= 67;
+    const c= 89;
+    console. log("this is Global Scope",a);
+    console. log("these are Block Scope variables",b,c);
+
+}
+
+demonstrateBlockScope();
+
+console.log(" 'a': ", a);
+console.log ("'b' and 'c' are not accessible here and will throw ReferenceError if accessed.");
