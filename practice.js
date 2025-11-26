7d70e7ababd5af46791e96f46f24d19ab455a27c
Ram Ghimire <ramghimire@192.168.0.108>
2025-11-26 21:52:49 +0545

    nothing

--- practice.js
+++ practice.js
@@ -1,8 +1,8 @@
-let a;
-const b =34;
+let a= 0;
+const b= 34;
  
 function shadowing (){
-    let a =45;
+    let a= 45;
     return console.log("Value of 'a' is shadowing the outer 'a': ", a);
 }
 shadowing();
